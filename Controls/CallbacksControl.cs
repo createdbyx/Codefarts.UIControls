e7252b48231098cf85e21c5d36f4093aed9351cf
Dean Lunz <deanlunz@createdbyx.com>
2015-03-19 12:53:56 -0700

    added callbacks control

---
+++ Controls/CallbacksControl.cs
@@ -0,0 +1,28 @@
+namespace Codefarts.UIControls
+{
+    using System;
+
+    public class CallbacksControl : CustomControl
+    {
+        public Action<IControlRendererManager, float, float> Draw;
+        public Action<IControlRendererManager, float, float> Update;
+
+        public override void OnDraw(IControlRendererManager manager, float elapsedGameTime, float totalGameTime)
+        {
+            var action = this.Draw;
+            if (action != null)
+            {
+                action(manager, elapsedGameTime, totalGameTime);
+            }
+        }
+
+        public override void OnUpdate(IControlRendererManager manager, float elapsedGameTime, float totalGameTime)
+        {
+            var action = this.Update;
+            if (action != null)
+            {
+                action(manager, elapsedGameTime, totalGameTime);
+            }
+        }
+    }
+}
