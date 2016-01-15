1ef418914902f75826a3f5a9699564f1e70b79ef
Dean Lunz <deanlunz@createdbyx.com>
2016-01-15 06:12:19 -0800

    added factory class for abstract factory mplementations

---
+++ Factories/UIControlsFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace Codefarts.UIControls.Factories
+{
+    /// <summary>
+    /// Provides the base factory implementation.
+    /// </summary>
+    public abstract class UIControlsFactory
+    {
+        /// <summary>
+        /// Gets or sets a singleton instance of a <see cref="UIControlsFactory"/>.
+        /// </summary>
+        public static UIControlsFactory Instance { get; set; }
+
+        /// <summary>
+        /// Creates a font.
+        /// </summary>
+        /// <param name="fontName">Name of the font.</param>
+        /// <param name="size">The size of the font text.</param>
+        /// <returns>A reference to a font class.</returns>
+        public abstract Font CreateFont(string fontName, float size);
+    }
+}
