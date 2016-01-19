d11b09e314a8188e712832ad455ce8cdcd2de22c
Dean Lunz <deanlunz@createdbyx.com>
2016-01-19 03:08:23 -0800

    Added point and size types

---
+++ Point.cs
@@ -0,0 +1,186 @@
+namespace Codefarts.UIControls
+{
+    using System;
+    using System.Globalization;
+
+#if UNITY_5
+    using UnityEngine;
+#endif
+
+    /// <summary>Represents an ordered pair of floating-point x- and y-coordinates that defines a point in a two-dimensional plane.</summary>
+    [Serializable]
+    public struct Point
+    {
+        /// <summary>Represents a new instance of the <see cref="Point" /> class with member data left uninitialized.</summary>              
+        public readonly static Point Empty;
+
+        /// <summary>
+        /// The value for the <see cref="X"/> property.
+        /// </summary>
+        private float x;
+
+        /// <summary>
+        /// The value for the <see cref="Y"/> property.
+        /// </summary>
+        private float y;
+
+        /// <summary>Gets a value indicating whether this <see cref="Point" /> is empty.</summary>
+        /// <returns>true if both <see cref="Point.X" /> and <see cref="Point.Y" /> are 0; otherwise, false.</returns>                       
+        public bool IsEmpty
+        {
+            get
+            {
+                if (Math.Abs(this.x) > float.Epsilon)
+                {
+                    return false;
+                }
+
+                return Math.Abs(this.y) < float.Epsilon;
+            }
+        }
+
+        /// <summary>Gets or sets the x-coordinate of this <see cref="Point" />.</summary>
+        /// <returns>The x-coordinate of this <see cref="Point" />.</returns>
+        public float X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        /// <summary>Gets or sets the y-coordinate of this <see cref="Point" />.</summary>
+        /// <returns>The y-coordinate of this <see cref="Point" />.</returns>   
+        public float Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+                 
+        /// <summary>Initializes a new instance of the <see cref="Point" /> class with the specified coordinates.</summary>
+        /// <param name="x">The horizontal position of the point. </param>
+        /// <param name="y">The vertical position of the point. </param>
+        public Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>Translates a given <see cref="Point" /> by the specified <see cref="Size" />.</summary>
+        /// <returns>The translated <see cref="Point" />.</returns>
+        /// <param name="pt">The <see cref="Point" /> to translate.</param>
+        /// <param name="sz">The <see cref="Size" /> that specifies the numbers to add to the coordinates of <paramref name="pt" />.</param>
+        public static Point Add(Point pt, Size sz)
+        {
+            return new Point(pt.X + sz.Width, pt.Y + sz.Height);
+        }             
+
+        /// <summary>Specifies whether this <see cref="Point" /> contains the same coordinates as the specified <see cref="T:System.Object" />.</summary>
+        /// <returns>This method returns true if <paramref name="obj" /> is a <see cref="Point" /> and has the same coordinates as this <see cref="Point" />.</returns>
+        /// <param name="obj">The <see cref="T:System.Object" /> to test. </param>  
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point))
+            {
+                return false;
+            }
+            var point = (Point)obj;
+            if (Math.Abs(point.X - this.X) > float.Epsilon || Math.Abs(point.Y - this.Y) > float.Epsilon)
+            {
+                return false;
+            }
+            return point.GetType() == this.GetType();
+        }
+
+        /// <summary>Returns a hash code for this <see cref="Point" /> structure.</summary>
+        /// <returns>An integer value that specifies a hash value for this <see cref="Point" /> structure.</returns>  
+        public override int GetHashCode()
+        {
+            var temp = this;
+            return (temp.X.GetHashCode() * 397) ^ temp.Y.GetHashCode();
+        }
+
+        /// <summary>Translates a <see cref="Point" /> by a given <see cref="Size" />.</summary>
+        /// <returns>Returns the translated <see cref="Point" />.</returns>
+        /// <param name="pt">The <see cref="Point" /> to translate. </param>
+        /// <param name="sz">A <see cref="Size" /> that specifies the pair of numbers to add to the coordinates of <paramref name="pt" />. </param>
+        public static Point operator +(Point pt, Size sz)
+        {
+            return Add(pt, sz);
+        }      
+
+        /// <summary>Compares two <see cref="Point" /> structures. The result specifies whether the values of the <see cref="Point.X" /> and <see cref="Point.Y" /> properties of the two <see cref="Point" /> structures are equal.</summary>
+        /// <returns>true if the <see cref="Point.X" /> and <see cref="Point.Y" /> values of the left and right <see cref="Point" /> structures are equal; otherwise, false.</returns>
+        /// <param name="left">A <see cref="Point" /> to compare. </param>
+        /// <param name="right">A <see cref="Point" /> to compare. </param>
+        public static bool operator ==(Point left, Point right)
+        {
+            if (Math.Abs(left.X - right.X) > float.Epsilon)
+            {
+                return false;
+            }
+            return Math.Abs(left.Y - right.Y) < float.Epsilon;
+        }
+
+        /// <summary>Determines whether the coordinates of the specified points are not equal.</summary>
+        /// <returns>true to indicate the <see cref="Point.X" /> and <see cref="Point.Y" /> values of <paramref name="left" /> and <paramref name="right" /> are not equal; otherwise, false. </returns>
+        /// <param name="left">A <see cref="Point" /> to compare.</param>
+        /// <param name="right">A <see cref="Point" /> to compare.</param>
+        public static bool operator !=(Point left, Point right)
+        {
+            return !(left == right);
+        }
+
+        /// <summary>Translates a <see cref="Point" /> by the negative of a given <see cref="Size" />.</summary>
+        /// <returns>The translated <see cref="Point" />.</returns>
+        /// <param name="pt">The <see cref="Point" /> to translate.</param>
+        /// <param name="sz">The <see cref="Size" /> that specifies the numbers to subtract from the coordinates of <paramref name="pt" />.</param>
+        public static Point operator -(Point pt, Size sz)
+        {
+            return Subtract(pt, sz);
+        }   
+
+        /// <summary>Translates a <see cref="Point" /> by the negative of a specified size.</summary>
+        /// <returns>The translated <see cref="Point" />.</returns>
+        /// <param name="pt">The <see cref="Point" /> to translate.</param>
+        /// <param name="sz">The <see cref="Size" /> that specifies the numbers to subtract from the coordinates of <paramref name="pt" />.</param>
+        public static Point Subtract(Point pt, Size sz)
+        {
+            return new Point(pt.X - sz.Width, pt.Y - sz.Height);
+        }                                                      
+
+        /// <summary>Converts this <see cref="Point" /> to a human readable string.</summary>
+        /// <returns>A string that represents this <see cref="Point" />.</returns>         
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture, "{{X={0}, Y={1}}}", this.x, this.y);
+        }
+
+        public static implicit operator Size(Point size)
+        {
+            return new Size(size.X, size.Y);
+        }
+
+#if UNITY_5
+        public static implicit operator Vector2(Point point)
+        {
+            return new Vector2(point.X, point.Y);
+        }   
+
+        public static implicit operator Point(Vector2 vector)
+        {
+            return new Point(vector.x, vector.y);
+        }
+#endif
+    }
+}
---
+++ Size.cs
@@ -0,0 +1,226 @@
+﻿namespace Codefarts.UIControls
+{
+    using System;
+    using System.Globalization;
+
+#if UNITY_5
+    using UnityEngine;
+#endif
+
+    /// <summary>Stores an ordered pair of integers, which specify a <see cref="Size.Height" /> and <see cref="Size.Width" />.</summary>
+    [Serializable]
+    public struct Size
+    {
+        /// <summary>Gets a <see cref="Size" /> structure that has a <see cref="Size.Height" /> and <see cref="Size.Width" /> value of 0. </summary>
+        /// <returns>A <see cref="Size" /> that has a <see cref="Size.Height" /> and <see cref="Size.Width" /> value of 0.</returns>
+        public readonly static Size Empty;
+
+        private float width;
+
+        private float height;
+
+        /// <summary>Gets or sets the vertical component of this <see cref="Size" /> structure.</summary>
+        /// <returns>The vertical component of this <see cref="Size" /> structure, typically measured in pixels.</returns>
+        public float Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        /// <summary>Tests whether this <see cref="Size" /> structure has width and height of 0.</summary>
+        /// <returns>This property returns true when this <see cref="Size" /> structure has both a width and height of 0; otherwise, false.</returns>
+        public bool IsEmpty
+        {
+            get
+            {
+                if (Math.Abs(this.width) > float.Epsilon)
+                {
+                    return false;
+                }
+                return Math.Abs(this.height) < float.Epsilon;
+            }
+        }
+
+        /// <summary>Gets or sets the horizontal component of this <see cref="Size" /> structure.</summary>
+        /// <returns>The horizontal component of this <see cref="Size" /> structure, typically measured in pixels.</returns>
+        public float Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Size" /> structure from the specified <see cref="Point" /> structure.</summary>
+        /// <param name="pt">The <see cref="Point" /> structure from which to initialize this <see cref="Size" /> structure. </param>
+        public Size(Point pt)
+        {
+            this.width = pt.X;
+            this.height = pt.Y;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Size" /> structure from the specified dimensions.</summary>
+        /// <param name="width">The width component of the new <see cref="Size" />. </param>
+        /// <param name="height">The height component of the new <see cref="Size" />. </param>
+        public Size(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Size" /> structure from the specified dimensions.</summary>
+        /// <param name="width">The width component of the new <see cref="Size" />. </param>
+        /// <param name="height">The height component of the new <see cref="Size" />. </param>
+        public Size(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>Adds the width and height of one <see cref="Size" /> structure to the width and height of another <see cref="Size" /> structure.</summary>
+        /// <returns>A <see cref="Size" /> structure that is the result of the addition operation.</returns>
+        /// <param name="sz1">The first <see cref="Size" /> structure to add.</param>
+        /// <param name="sz2">The second <see cref="Size" /> structure to add.</param>
+        public static Size Add(Size sz1, Size sz2)
+        {
+            return new Size(sz1.Width + sz2.Width, sz1.Height + sz2.Height);
+        }
+
+        /// <summary>Converts the specified <see cref="Size" /> structure to a <see cref="Size" /> structure by rounding the values of the <see cref="Size" /> structure to the next higher integer values.</summary>
+        /// <returns>The <see cref="Size" /> structure this method converts to.</returns>
+        /// <param name="value">The <see cref="Size" /> structure to convert. </param>
+        public static Size Ceiling(Size value)
+        {
+            return new Size((float)Math.Ceiling(value.Width), (float)Math.Ceiling(value.Height));
+        }
+
+        /// <summary>Tests to see whether the specified object is a <see cref="Size" /> structure with the same dimensions as this <see cref="Size" /> structure.</summary>
+        /// <returns>true if <paramref name="obj" /> is a <see cref="Size" /> and has the same width and height as this <see cref="Size" />; otherwise, false.</returns>
+        /// <param name="obj">The <see cref="T:System.Object" /> to test. </param>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Size))
+            {
+                return false;
+            }
+            var size = (Size)obj;
+            if (Math.Abs(size.width - this.width) > float.Epsilon)
+            {
+                return false;
+            }
+            return Math.Abs(size.height - this.height) < float.Epsilon;
+        }
+
+
+        /// <summary>Returns a hash code for this <see cref="Size" /> structure.</summary>
+        /// <returns>An integer value that specifies a hash value for this <see cref="Size" /> structure.</returns>
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                var temp = this;
+                return (temp.Width.GetHashCode() * 397) ^ temp.Height.GetHashCode();
+            }
+        }
+
+        /// <summary>Adds the width and height of one <see cref="Size" /> structure to the width and height of another <see cref="Size" /> structure.</summary>
+        /// <returns>A <see cref="Size" /> structure that is the result of the addition operation.</returns>
+        /// <param name="sz1">The first <see cref="Size" /> to add. </param>
+        /// <param name="sz2">The second <see cref="Size" /> to add. </param>
+        public static Size operator +(Size sz1, Size sz2)
+        {
+            return Add(sz1, sz2);
+        }
+
+        /// <summary>Tests whether two <see cref="Size" /> structures are equal.</summary>
+        /// <returns>true if <paramref name="sz1" /> and <paramref name="sz2" /> have equal width and height; otherwise, false.</returns>
+        /// <param name="sz1">The <see cref="Size" /> structure on the left side of the equality operator. </param>
+        /// <param name="sz2">The <see cref="Size" /> structure on the right of the equality operator. </param>
+        public static bool operator ==(Size sz1, Size sz2)
+        {
+            if (Math.Abs(sz1.Width - sz2.Width) > float.Epsilon)
+            {
+                return false;
+            }
+            return Math.Abs(sz1.Height - sz2.Height) < float.Epsilon;
+        }
+
+        public static implicit operator Point(Size size)
+        {
+            return new Point(size.Width, size.Height);
+        }
+
+#if UNITY_5
+        public static implicit operator Vector2(Size size)
+        {
+            return new Vector2(size.Width, size.Height);
+        }
+ 
+        public static implicit operator Size(Vector2 vector)
+        {
+            return new Size(vector.x, vector.y);
+        }
+#endif
+
+        /// <summary>Tests whether two <see cref="Size" /> structures are different.</summary>
+        /// <returns>true if <paramref name="sz1" /> and <paramref name="sz2" /> differ either in width or height; false if <paramref name="sz1" /> and <paramref name="sz2" /> are equal.</returns>
+        /// <param name="sz1">The <see cref="Size" /> structure on the left of the inequality operator. </param>
+        /// <param name="sz2">The <see cref="Size" /> structure on the right of the inequality operator. </param>
+        public static bool operator !=(Size sz1, Size sz2)
+        {
+            return !(sz1 == sz2);
+        }
+
+        /// <summary>Subtracts the width and height of one <see cref="Size" /> structure from the width and height of another <see cref="Size" /> structure.</summary>
+        /// <returns>A <see cref="Size" /> structure that is the result of the subtraction operation.</returns>
+        /// <param name="sz1">The <see cref="Size" /> structure on the left side of the subtraction operator. </param>
+        /// <param name="sz2">The <see cref="Size" /> structure on the right side of the subtraction operator. </param>
+        /// <filterpriority>3</filterpriority>
+        public static Size operator -(Size sz1, Size sz2)
+        {
+            return Subtract(sz1, sz2);
+        }
+
+        /// <summary>Converts the specified <see cref="Size" /> structure to a <see cref="Size" /> structure by rounding the values of the <see cref="Size" /> structure to the nearest integer values.</summary>
+        /// <returns>The <see cref="Size" /> structure this method converts to.</returns>
+        /// <param name="value">The <see cref="Size" /> structure to convert. </param>
+        public static Size Round(Size value)
+        {
+            return new Size((float)Math.Round(value.Width), (float)Math.Round(value.Height));
+        }
+
+        /// <summary>Subtracts the width and height of one <see cref="Size" /> structure from the width and height of another <see cref="Size" /> structure.</summary>
+        /// <returns>A <see cref="Size" /> structure that is a result of the subtraction operation.</returns>
+        /// <param name="sz1">The <see cref="Size" /> structure on the left side of the subtraction operator. </param>
+        /// <param name="sz2">The <see cref="Size" /> structure on the right side of the subtraction operator. </param>
+        public static Size Subtract(Size sz1, Size sz2)
+        {
+            return new Size(sz1.Width - sz2.Width, sz1.Height - sz2.Height);
+        }
+
+        /// <summary>Creates a human-readable string that represents this <see cref="Size" /> structure.</summary>
+        /// <returns>A string that represents this <see cref="Size" />.</returns>
+        public override string ToString()
+        {
+            return string.Concat("{Width=", this.width.ToString(CultureInfo.CurrentCulture), ", Height=", this.height.ToString(CultureInfo.CurrentCulture), "}");
+        }
+
+        /// <summary>Converts the specified <see cref="Size" /> structure to a <see cref="Size" /> structure by truncating the values of the <see cref="Size" /> structure to the next lower integer values.</summary>
+        /// <returns>The <see cref="Size" /> structure this method converts to.</returns>
+        /// <param name="value">The <see cref="Size" /> structure to convert. </param>
+        public static Size Truncate(Size value)
+        {
+            return new Size((int)value.Width, (int)value.Height);
+        }
+    }
+}
