5cb549d9de6e4bed269ae4568c60f031b9b483a4
Dean Lunz <deanlunz@createdbyx.com>
2016-01-21 05:11:53 -0800

    added appearance enum

---
+++ Controls/Appearance.cs
@@ -0,0 +1,18 @@
+namespace Codefarts.UIControls
+{
+    /// <summary>
+    /// Specifies the appearance of a control.
+    /// </summary>
+    public enum Appearance
+    {
+        /// <summary>
+        /// The default appearance defined by the control class.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// The appearance of a Windows button.
+        /// </summary>
+        Button
+    }
+}
