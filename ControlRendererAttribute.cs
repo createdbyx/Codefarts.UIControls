e187c057dbcfb4a2e4f9ca4a8148ce2012d27a15
Dean Lunz <deanlunz@createdbyx.com>
2016-01-29 05:58:11 -0800

    added control renderer attribute

---
+++ ControlRendererAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace Codefarts.UIControls
+{
+    using System;
+
+    /// <summary>
+    /// Provides a attribite for informing the renderer manager what renderer to use.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ControlRendererAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Attribute"/> class.
+        /// </summary>
+        public ControlRendererAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Attribute" /> class.
+        /// </summary>
+        /// <param name="type">The type that should be used to render the control.</param>
+        public ControlRendererAttribute(Type type)
+        {
+            this.FullName = type.FullName;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Attribute" /> class.
+        /// </summary>
+        /// <param name="fullName">The <see cref="Type.FullName"/> of the type that should be used to render the control.</param>
+        public ControlRendererAttribute(string fullName)
+        {
+            this.FullName = fullName;
+        }
+
+        /// <summary>
+        /// Gets or sets the full name of the type.
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
