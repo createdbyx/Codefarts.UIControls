f03100588c0a1be7251c218bfdad94d8c696722a
Dean Lunz <deanlunz@createdbyx.com>
2016-02-03 14:06:39 -0800

    added xml documentation

--- Controls/CallbacksControl.cs
+++ Controls/CallbacksControl.cs
@@ -2,11 +2,26 @@ namespace Codefarts.UIControls
 {
     using System;
 
+    /// <summary>
+    /// Provides a custom rendering control that uses callbacks to draw and update.
+    /// </summary>
     public class CallbacksControl : CustomControl
     {
+        /// <summary>
+        /// The draw callback.
+        /// </summary>
         public Action<ControlRenderingArgs> Draw;
+
+        /// <summary>
+        /// The update callback.
+        /// </summary>
         public Action<ControlRenderingArgs> Update;
 
+
+        /// <summary>
+        /// Call this method when the control needs to draw it self.
+        /// </summary>
+        /// <param name="args">The rendering arguments.</param>
         public override void OnDraw(ControlRenderingArgs args)
         {
             var action = this.Draw;
@@ -16,6 +31,10 @@ namespace Codefarts.UIControls
             }
         }
 
+        /// <summary>
+        /// Call this method when the contrl needs to draw it self.
+        /// </summary>
+        /// <param name="args">The update arguments.</param>
         public override void OnUpdate(ControlRenderingArgs args)
         {
             var action = this.Update;
--- Controls/CustomControl.cs
+++ Controls/CustomControl.cs
@@ -9,9 +9,21 @@
 
 namespace Codefarts.UIControls
 {
+    /// <summary>
+    /// Provides an abstract control that does it's own rendering & updating.
+    /// </summary>
     public abstract class CustomControl : Control
     {
+        /// <summary>
+        /// Call this method when the control needs to draw it self.
+        /// </summary>
+        /// <param name="args">The rendering arguments.</param>
         public abstract void OnDraw(ControlRenderingArgs args);
+
+        /// <summary>
+        /// Call this method when the contrl needs to draw it self.
+        /// </summary>
+        /// <param name="args">The update arguments.</param>
         public abstract void OnUpdate(ControlRenderingArgs args);
     }
 }
