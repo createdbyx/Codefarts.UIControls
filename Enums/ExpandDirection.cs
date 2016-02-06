0bf1c5d47e0e7339a357e3365b51677f6362dc11
Dean Lunz <deanlunz@createdbyx.com>
2016-02-05 16:40:26 -0800

    Moved enum code files to Enums folder

---
+++ Enums/ExpandDirection.cs
@@ -0,0 +1,28 @@
+namespace Codefarts.UIControls
+{
+    /// <summary>
+    /// Specifies the direction in which an <see cref="Expander" /> control opens. 
+    /// </summary>
+    public enum ExpandDirection
+    {
+        /// <summary>
+        /// The <see cref="Expander" /> control opens down.
+        /// </summary>
+        Down,
+
+        /// <summary>
+        /// The <see cref="Expander" /> control opens up.
+        /// </summary>
+        Up,
+
+        /// <summary>
+        /// The <see cref="Expander" /> control opens left.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// The <see cref="Expander" /> control opens right. 
+        /// </summary>
+        Right
+    }
+}
---
+++ Enums/SelectionMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Codefarts.UIControls
+{
+    /// <summary>
+    /// Defines the selection behavior for a <see cref="ListBox" />.
+    /// </summary>
+    public enum SelectionMode
+    {
+        /// <summary>
+        /// The user can select only one item at a time. 
+        /// </summary>
+        Single,
+
+        /// <summary>
+        /// The user can select multiple items without holding down a modifier key.
+        /// </summary>
+        Multiple,
+
+        /// <summary>
+        /// The user can select multiple consecutive items while holding down the SHIFT key.
+        /// </summary>
+        Extended
+    }
+}
