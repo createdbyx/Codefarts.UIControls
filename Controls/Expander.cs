be2d854aeb466c58fd5d97978dca64d9197cc63e
Dean Lunz <deanlunz@createdbyx.com>
2016-02-05 16:47:32 -0800

    full code cleanup of code and included xml documentation

--- Controls/Expander.cs
+++ Controls/Expander.cs
@@ -7,21 +7,96 @@
 </copyright>
 */
 
-
 namespace Codefarts.UIControls
 {
-    using System.Collections.Generic;
-
+    /// <summary>
+    /// Represents the control that displays a header that has a collapsible content.
+    /// </summary>
     public class Expander : Control
     {
-        public bool IsExpanded { get; set; }
-        public IList<Control> Children { get; set; }
-        public string Header { get; set; }
+        /// <summary>
+        /// The backing field for the <see cref="Text"/> property.
+        /// </summary>
+        protected string text = string.Empty;
 
-        public Expander()
+        /// <summary>
+        /// The backing field for the <see cref="IsExpanded"/> property.
+        /// </summary>
+        protected bool isExpanded;
+
+        /// <summary>
+        /// The backing field for the <see cref="ExpandDirection"/> property.
+        /// </summary>
+        protected ExpandDirection expandDirection;
+
+
+        /// <summary>
+        /// Gets or sets the direction in which the <see cref="Expander" /> content opens.  
+        /// </summary>
+        /// <returns>
+        /// One of the <see cref="ExpandDirection" /> values that defines which direction the content opens. The default is <see cref="ExpandDirection.Down" />. 
+        /// </returns>
+        public ExpandDirection ExpandDirection
         {
-            this.Header = string.Empty;
-            this.Children = new List<Control>();
+            get
+            {
+                return this.expandDirection;
+            }
+
+            set
+            {
+                var changed = this.expandDirection != value;
+                this.expandDirection = value;
+                if (changed)
+                {
+                    this.OnPropertyChanged("ExpandDirection");
+                }
+            }
         }
+
+        /// <summary>
+        /// Gets or sets whether the <see cref="Expander" /> content is visible.  
+        /// </summary>
+        /// <returns>
+        /// true if the content is expanded; otherwise, false. The default is false.
+        /// </returns>
+        public virtual bool IsExpanded
+        {
+            get
+            {
+                return this.isExpanded;
+            }
+
+            set
+            {
+                var changed = this.isExpanded != value;
+                this.isExpanded = value;
+                if (changed)
+                {
+                    this.OnPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the header text.
+        /// </summary>                  
+        public virtual string Text
+        {
+            get
+            {
+                return this.text;
+            }
+
+            set
+            {
+                var changed = this.text != value;
+                this.text = value;
+                if (changed)
+                {
+                    this.OnPropertyChanged("Text");
+                }
+            }
+        }     
     }
 }
--- Controls/ProgressBar.cs
+++ Controls/ProgressBar.cs
@@ -1,15 +1,26 @@
 namespace Codefarts.UIControls
-{            
+{
+    /// <summary>
+    /// Provides a progres bar control.
+    /// </summary>
     public class ProgressBar : RangeBase
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ProgressBar"/> class.
         /// </summary>
         public ProgressBar()
-            : base()
         {
             this.Foreground = new SolidColorBrush(new Color(0, 1, 0));
             this.Maximum = 100;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressBar"/> class.
+        /// </summary>
+        /// <param name="name">The name of the control.</param>
+        public ProgressBar(string name) : this()
+        {
+            this.name = name;
         }
     }
 }
