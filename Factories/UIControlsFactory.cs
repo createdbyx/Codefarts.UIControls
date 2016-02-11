f8a51143ebdf0aea5551dd38807ad267caa386d8
Dean Lunz <deanlunz@createdbyx.com>
2016-02-10 23:56:18 -0800

    UIControlsFactory changes somewhat Instance is now DefaultManager Added GetManager method to get manager responsible for creating various platform specific objects

--- Factories/UIControlsFactory.cs
+++ Factories/UIControlsFactory.cs
@@ -1,44 +1,23 @@
 ﻿namespace Codefarts.UIControls.Factories
 {
-    using System.IO;
-
     /// <summary>
     /// Provides the base factory implementation.
     /// </summary>
     public abstract class UIControlsFactory
     {
         /// <summary>
-        /// Gets or sets a singleton instance of a <see cref="UIControlsFactory"/>.
+        /// Gets or sets a singleton instance of a <see cref="IUIControlsManager"/>.
         /// </summary>
-        public static UIControlsFactory Instance { get; set; }
+        public static IUIControlsManager DefaultManager { get; set; }
 
         /// <summary>
-        /// Creates a font.
+        /// Gets a <see cref="IUIControlsManager"/> manager instance.
         /// </summary>
-        /// <param name="fontName">Name of the font.</param>
-        /// <param name="size">The size of the font text.</param>
-        /// <returns>A reference to a font class.</returns>
-        public abstract Font CreateFont(string fontName, float size);
-
-        /// <summary>
-        /// Creates a bitmap source.
-        /// </summary>
-        /// <returns>A reference to a <see cref="BitmapSource"/> type.</returns>
-        public abstract ImageSource CreateImageSource();
-
-        /// <summary>
-        /// Creates a bitmap source.
-        /// </summary>
-        /// <param name="width">The bitmap width.</param>
-        /// <param name="height">The bitmap height.</param>
-        /// <returns>A reference to a <see cref="BitmapSource"/> type.</returns>
-        public abstract ImageSource CreateImageSource(int width, int height);
-
-        /// <summary>
-        /// Creates a bitmap source.
-        /// </summary>
-        /// <param name="stream">The stream where the bitmap will be read from.</param>
-        /// <returns>A reference to a <see cref="BitmapSource"/> type.</returns>
-        public abstract ImageSource CreateImageSource(Stream stream);
+        /// <param name="UniqueId">The unique identifier for the manager.</param>
+        /// <returns>A reference to a <see cref="IUIControlsManager"/> implementation, otherwise null if unable to get a manager.</returns>
+        public static IUIControlsManager GetManager(string UniqueId)
+        {
+            return null;
+        }
     }
 }
