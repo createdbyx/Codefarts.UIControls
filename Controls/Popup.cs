ae4dc5c34710138dce63f415c388107d25c24637
Dean Lunz <deanlunz@createdbyx.com>
2016-02-27 01:36:44 -0800

    added popup control and toolstrip

---
+++ Controls/Popup.cs
@@ -0,0 +1,33 @@
+namespace Codefarts.UIControls
+{
+    using System;
+
+    /// <summary>
+    /// Used to group collections of controls.
+    /// </summary>
+    public class Popup : Panel
+    {
+        public void Show(Control owner)
+        {
+            if (owner == null)
+            {
+                throw new ArgumentNullException("owner");
+            }
+
+            // get root control
+            var root = owner;
+            while (root.Parent != null)
+            {
+                root = root.Parent;
+            }
+
+            var ownerOrigin = owner.PointToScreen(Point.Empty);
+            this.Location = ownerOrigin + new Point(0, owner.Height) - root.Location;
+
+            // perform checking weather or not popup is within 
+            this.Visibility = Visibility.Visible;
+
+            root.Controls.Add(this);
+        }
+    }
+}
---
+++ Controls/ToolStrip.cs
@@ -0,0 +1,9 @@
+namespace Codefarts.UIControls.Controls
+{
+    /// <summary>
+    /// Provides a container for toolbar objects. 
+    /// </summary>
+    public class ToolStrip : Control
+    {
+    }
+}
