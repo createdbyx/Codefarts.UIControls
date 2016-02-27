d5478d945394d5d042da10893067ca690f2bc59a
Dean Lunz <deanlunz@createdbyx.com>
2016-02-27 01:35:03 -0800

    enum updates

---
+++ Enums/MouseEventType.cs
@@ -0,0 +1,33 @@
+namespace Codefarts.UIControls
+{
+    /// <summary>
+    /// Provides a enum describing a mouse event.
+    /// </summary>
+    public enum MouseEventType
+    {
+        /// <summary>
+        /// Mouse enter event.
+        /// </summary>
+        MouseEnter,
+
+        /// <summary>
+        /// Mouse leave event.
+        /// </summary>
+        MouseLeave,
+
+        /// <summary>
+        /// Mouse move event.
+        /// </summary>
+        MouseMove,
+
+        /// <summary>
+        /// Mouse down event.
+        /// </summary>
+        MouseDown,
+
+        /// <summary>
+        /// Mouse up event.
+        /// </summary>
+        MouseUp
+    }
+}
