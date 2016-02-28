0e7330c36c1dec50dcc509e42408965da40a571f
Dean Lunz <deanlunz@createdbyx.com>
2016-02-28 12:58:30 -0800

    Added a Constants class for housing various constants

---
+++ Constants.cs
@@ -0,0 +1,38 @@
+namespace Codefarts.UIControls
+{
+    /// <summary>
+    /// Provices various constants used by UIControls.
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// The maximum number of supported mouse buttons.
+        /// </summary>
+        public const int MaxMouseButtons = 20;
+
+        /// <summary>
+        /// The left mouse button index;
+        /// </summary>
+        public const int LeftMouseButton = 0;
+        
+        /// <summary>
+        /// The right mouse button index.
+        /// </summary>
+        public const int RightMouseButton = 1;
+        
+        /// <summary>
+        /// The middle mouse button index.
+        /// </summary>
+        public const int MiddleMouseButton = 2;
+
+        /// <summary>
+        /// The index for mouse button 4.
+        /// </summary>
+        public const int MouseButton4 = 3;
+
+        /// <summary>
+        /// The index for mouse button 5.
+        /// </summary>
+        public const int MouseButton5 = 4;
+    }
+}
