f0f1a4c60d2b659009547c63b44a1f6badb9ae58
Dean Lunz <deanlunz@createdbyx.com>
2016-03-01 01:28:18 -0800

    documentation comments

--- Brushes/SkinningBrush.cs
+++ Brushes/SkinningBrush.cs
@@ -11,11 +11,25 @@ namespace Codefarts.UIControls
         /// Gets or sets the normal rect in texture cordanants 0 to 1.
         /// </summary>
         public Rect Normal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Active rect in texture cordanants 0 to 1.
+        /// </summary>
         public Rect Active { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Focused rect in texture cordanants 0 to 1.
+        /// </summary>
         public Rect Focused { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Inactive rect in texture cordanants 0 to 1.
+        /// </summary>
         public Rect Inactive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Hover rect in texture cordanants 0 to 1.
+        /// </summary>
         public Rect Hover { get; set; }
-
-
     }
 }
