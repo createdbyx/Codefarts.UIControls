95bbc058a724dd4bb5209b4add797c0e934add38
Dean Lunz <deanlunz@createdbyx.com>
2016-03-02 19:32:15 -0800

    xml documentation

--- Enums/Visibility.cs
+++ Enums/Visibility.cs
@@ -9,14 +9,24 @@
 
 namespace Codefarts.UIControls
 {
-    /// <summary>Specifies the display state of an element. </summary>
+    /// <summary>
+    /// Specifies the display state of an element. 
+    /// </summary>
     public enum Visibility : byte
     {
-        /// <summary>Display the element.</summary>
+        /// <summary>
+        /// Display the element.
+        /// </summary>
         Visible,
-        /// <summary>Do not display the element, but reserve space for the element in layout.</summary>
+
+        /// <summary>
+        /// Do not display the element, but reserve space for the element in layout.
+        /// </summary>
         Hidden,
-        /// <summary>Do not display the element, and do not reserve space for it in layout.</summary>
+
+        /// <summary>
+        /// Do not display the element, and do not reserve space for it in layout.
+        /// </summary>
         Collapsed
     }
 }
