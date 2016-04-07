e099a537c86b740ed5db17d1e471bfc8de51ed75
Dean Lunz <deanlunz@createdbyx.com>
2016-04-07 04:24:21 -0700

    moved collection classes into a collections folder and added SelectedIndexCollection

---
+++ Collections/ItemsCollection.cs
@@ -0,0 +1,21 @@
+/*
+<copyright>
+  Copyright (c) 2012 Codefarts
+  All rights reserved.
+  contact@codefarts.com
+  http://www.codefarts.com
+</copyright>
+*/
+
+namespace Codefarts.UIControls
+{
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// Provides a collection of <see cref="object"/> types.
+    /// </summary>
+    /// <remarks>This collection is used by the <see cref="ListBox"/> control.</remarks>
+    public class ItemsCollection : ObservableCollection<object>
+    {                                           
+    }
+}
--- Collections/SelectedIndexCollection.cs
+++ Collections/SelectedIndexCollection.cs
@@ -12,11 +12,10 @@ namespace Codefarts.UIControls
     using System.Collections.ObjectModel;
 
     /// <summary>
-    /// Provides a collection of <see cref="object"/> types.
+    /// Provides a collection of <see cref="int"/> types.
     /// </summary>
     /// <remarks>This collection is used by the <see cref="ListBox"/> control.</remarks>
-    /// <seealso cref="object" />
-    public class ItemsCollection : ObservableCollection<object>
-    {                                           
+    public class SelectedIndexCollection : ObservableCollection<int>
+    {
     }
 }
