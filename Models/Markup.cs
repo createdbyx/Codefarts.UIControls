634bb36259016df66accd36436a7ddc33b538887
Dean Lunz <deanlunz@createdbyx.com>
2016-06-17 01:08:37 -0700

    xml documentation

--- Models/Markup.cs
+++ Models/Markup.cs
@@ -2,10 +2,24 @@ namespace Codefarts.UIControls.Models
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Provides a model for markup conversion.
+    /// </summary>
     public class Markup
     {
+        /// <summary>
+        /// Gets or sets the name associated with the markup element.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the markup children.
+        /// </summary>
         public IList<Markup> Children { get; set; }
+
+        /// <summary>
+        /// Gets or sets the markup element properties.
+        /// </summary>
         public IDictionary<string, object> Properties { get; set; }
 
         /// <summary>
