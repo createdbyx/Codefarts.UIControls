ed56c3cdc0075934cfb7533c68fc73a33033a243
Dean Lunz <createdbyx@createdbyx.com>
2017-08-13 23:03:13 -0700

    added user control constructors

--- Controls/UserControl.cs
+++ Controls/UserControl.cs
@@ -8,5 +8,21 @@
     /// <seealso cref="Codefarts.UIControls.Control" />
     public class UserControl : Control
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserControl"/> class.
+        /// </summary>
+        public UserControl()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserControl"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name for the control.
+        /// </param>
+        public UserControl(string name)  :base(name)
+        {
+        }
     }
 }
